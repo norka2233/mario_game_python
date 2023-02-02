7b103f94c28c9745971eed3d1341a360af301dfd
Eleonora <norka2233@gmail.com>
2023-02-02 14:30:50 +0200

    soundtrack

---
+++ monsters.py
@@ -0,0 +1,1 @@
+import osfrom pygame import *from settings import MONSTER_WIDTH, MONSTER_COLOR, MONSTER_HEIGHT, ICON_DIRclass Monster(sprite.Sprite):    def __init__(self, x, y, left, up, max_length_left, max_length_up ):        pass    def update(self):        pass    def collide(self, platform):        pass
